2f92255b46976e12cadb45f83d5c5dd319dd9077
♥ыыы)♥ <ambal.vpn@gmail.com>
2022-05-16 15:45:45 +0400

    in

--- main.py
+++ main.py
@@ -1,42 +1,40 @@
 import sys
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import (QWidget, QLabel, QLineEdit, QGridLayout, QApplication, QPushButton, QDateEdit)
+from PyQt5.QtWidgets import (QWidget, QMainWindow, QLabel, QLineEdit, QGridLayout, QApplication, QPushButton, QDateEdit,
+                             QFormLayout)
 
 
 class Example(QWidget):
 
-    def __init__(self):
-        super().__init__()
-        self.initUI()
+    def __init__(self, parent=None):
+        super().__init__(parent)
 
-    def initUI(self):
-        doc = QLabel('ФИО врача')
-        pat = QLabel('ФИО пациента')
-        date = QLabel('дата рождения')
         self.docEdit = QLineEdit()
         self.patEdit = QLineEdit()
         self.dateEdit = QDateEdit()
         addDataButton = QPushButton("Показать данные")
         addDataButton.clicked.connect(self.showData)
-        grid = QGridLayout()
+        grid = QFormLayout()
         grid.setSpacing(1)
 
-        grid.addWidget(doc, 1, 0)
-        grid.addWidget(self.docEdit, 1, 1)
+        grid.addRow('ФИО врача', self.docEdit)
+        # grid.addWidget(doc, 1, 0)
+        # grid.addWidget(self.docEdit, 1, 1)
 
-        grid.addWidget(pat, 2, 0)
-        grid.addWidget(self.patEdit, 2, 1)
+        grid.addRow('ФИО  пациента', self.patEdit)
+        # grid.addWidget(pat, 2, 0)
+        # grid.addWidget(self.patEdit, 2, 1)
 
-        grid.addWidget(date, 3, 0)
-        grid.addWidget(self.dateEdit, 3, 1)
+        grid.addRow(' дата рождения', self.dateEdit)
+        # grid.addWidget(date, 3, 0)
+        # grid.addWidget(self.dateEdit, 3, 1)
 
-        grid.addWidget(addDataButton, 4, 0, 1, 5)
+        grid.addRow(addDataButton)
 
         self.setLayout(grid)
 
         self.setGeometry(200, 200, 350, 300)
         self.setWindowTitle('Test')
-        self.show()
 
     def showData(self):
         print("ФИО врача - %s\nФИО пациента - %s\nДата рождения: %s" % (
